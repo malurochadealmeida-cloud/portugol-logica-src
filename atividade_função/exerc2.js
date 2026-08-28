886a0535602eee9b11efe640ba13681b34c534d4
Malu <malurochadealmeida@gmail.com>
2026-08-28 20:15:53 -0300

    Add files via upload

---
+++ atividade_função/exerc2.js
@@ -0,0 +1,5 @@
+function mediaAlunos(nota1,nota2,nota3){
+    return (nota1+nota2+nota3) /3
+
+}
+console.log("Sua média é: " ,mediaAlunos(8,9,10))
---
+++ atividade_função/exerci3.js
@@ -0,0 +1,11 @@
+function calcularArea(base,altura){
+        return base*altura
+
+}
+function calcularPerimetro (l1,l2,l3){
+
+return l1+l2+l3
+}
+
+console.log("A área do seu retângulo é: " +calcularArea(25,8))
+console.log("A área do seu triângulo é: " +calcularPerimetro(15,10,5))
